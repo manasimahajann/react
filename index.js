41e736ac28a794190fc3d4a687ac9c2678e75c85
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2022-01-18 20:33:28 -0500

    render component learning!

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-ReactDOM.render(<h1>Yo!!!..............</h1>, document.getElementById('root'))
+const page = (
+    <div className = "navbar">
+           <nav>
+        <a href="#">Home</a> |
+        <a href="#">Interview</a> |
+        <a href="#">Languages</a> |
+        <a href="#">Data Structure</a> |
+        <a href="#">Algorithm</a>
+        <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+
+
+        </ul>
+    </nav>
+    <h1>Hiking, a ritual</h1>
+    </div>
+)
+
+console.log(page)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
