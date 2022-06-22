666720ad6b77a480ff9b44423b20ad4a34e80e55
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2022-06-22 16:29:11 -0400

    basic

---
+++ react-course/src/App.js
@@ -0,0 +1,15 @@
+ 
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+        <h1>My todos</h1>
+        <Todo text= 'first learn'></Todo>
+        <Todo text= 'next learn more'></Todo>
+        <Todo text= 'decide!!!'></Todo>
+    </div> 
+  );
+}
+
+export default App;
