9d0907542ae08152ef80ff54c81ab5f63052a3dc
Manasi Mahajan <manasi@paymentsolutionsinc.net>
2022-06-23 23:29:40 -0400

    newmeetupappreactcourse

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,21 @@
- 
-import Todo from "./components/Todo";
+  
+
+import React from "react"; 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import AllMeetupsPage from './Pages/AllMeetupsPage';
+import FavoritesPage from './Pages/FavoritesPage';
+import NewMeetupPage from './Pages/NewMeetupPage';
 
 function App() {
   return (
-    <div>
-        <h1>My todos</h1>
-        <Todo text= 'first learn'></Todo>
-        <Todo text= 'next learn more'></Todo>
-        <Todo text= 'decide!!!'></Todo>
-    </div> 
+ 
+    <Routes>
+      <Route path="/all_meetup" element={<AllMeetupsPage />} />
+      <Route path="/new_meetup" element={<NewMeetupPage />} />
+
+    </Routes>
+       
   );
 }
 
